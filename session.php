f0db969b7bf23510918a837a5443d3abf8badde7
Zerina Dragnic <zdragnic@gmail.com>
2016-12-30 20:43:09 +0100

    promjena

--- session.php
+++ session.php
@@ -3,7 +3,7 @@ session_start();
 $user_check=$_SESSION['login_user'];
 $login_session =$user_check;
 if(!isset($login_session)){
-header('Location: pocetna.php');
+header('Location: index.php');
 }
 
 ?>
